3ccacaf78d54c426a27eea98ad4e7d763b1bc1c6
Lin  Peng <u6071322@anu.edu.au>
2017-06-17 20:27:56 +1000

    first commit

--- app.js
+++ app.js
@@ -1,59 +1,110 @@
-$(document).ready(function () {
+var bufferLength;
+var dataArray;
+var player;
+function initPage(){
 
+  alert("Initializing page...");
+  player =  document.getElementById('myAudio');
+  createAudio(player);
+  player.play();
+
+}
+
+function createAudio(player){
+  var self = this;
   var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-  var audioElement = document.getElementById('audioElement');
-  var audioSrc = audioCtx.createMediaElementSource(audioElement);
   var analyser = audioCtx.createAnalyser();
+  analyser.fftSize = 256;
+  bufferLength = analyser.frequencyBinCount;
+  dataArray = new Uint8Array(bufferLength);
+  analyser.getByteTimeDomainData(dataArray); // get waveform data and put it into the array created above
+  console.log("dataArray: " + dataArray)
+  console.log(bufferLength)
+  console.log(analyser)
 
-  // Bind our analyser to the media element source.
-  audioSrc.connect(analyser);
-  audioSrc.connect(audioCtx.destination);
 
-  //var frequencyData = new Uint8Array(analyser.frequencyBinCount);
-  var frequencyData = new Uint8Array(200);
+};
 
-  var svgHeight = '300';
-  var svgWidth = '1200';
-  var barPadding = '1';
 
-  function createSvg(parent, height, width) {
-    return d3.select(parent).append('svg').attr('height', height).attr('width', width);
+var canvas = document.getElementById("myCanvas");
+var c = canvas.getContext("2d");
+
+//create te container that will hold the boincing balls.
+var container = {
+  x: 0,
+  y: 0,
+  width: 700,
+  height: 300
+};
+//create the array of circles that will be animated
+var circles = [{
+  x: 20,
+  y: 100,
+  r: 10,
+  vx: 10,
+  vy: 9,
+  color: 125
+}];
+
+function animate() {
+var z;
+var v;
+
+
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  //draw the container
+  c.fillStyle = "rgba(0, 0, 200, 0)";
+  c.fillRect(container.x, container.y, container.width, container.height);
+
+  //loop throughj the circles array
+  for (var i = 0; i < circles.length; i++) {
+    //draw the circles
+    c.fillStyle = 'hsl(' + circles[0].color++ + ', 50%, 60%)';
+    c.beginPath();
+    c.arc(circles[0].x, circles[0].y, circles[0].r, 0, Math.PI * 2, true);
+    c.fill()
+  } // first loop
+
+  for(var j = 0; j < bufferLength; j++) {
+    v = dataArray[j] / 128.0;
+    // console.log("V:" + v);
+    z = v * (circles[0].y/2);
+    // console.log(z);
+    vx = v*z;
+    // console.log("vx:" + vx);
+    //
+    //time to animate our circles ladies and gentlemen.
+    if (circles[0].x - circles[0].r + circles[0].vx < container.x || circles[0].x + circles[0].r + circles[0].vx > container.x + container.width) {
+        circles[0].vx = -circles[0].vx;
+
+    }
+
+    if (circles[0].y + circles[0].r + circles[0].vy > container.y + container.height || circles[0].y - 10 + circles[0].vy < container.y) {
+      circles[0].vy = -circles[0].vy;
+    }
+
+    circles[0].x += circles[0].vx;
+    circles[0].y += circles[0].vy;
+    v += sliceWidth;
+
   }
+  requestAnimationFrame(animate);
+}
+//
+// var v;
+// var z;
+// var vx;
+//
+// function draw(){
+//   requestAnimationFrame(draw);
+//   c.clearRect(0, 0, canvas.width, canvas.height);
+//   c.fillStyle = 'hsl(' + circles[0].color++ + ', 50%, 60%)';
+//   c.beginPath();
+//   c.arc(circles[0].x, circles[0].y, circles[0].r, 0, Math.PI * 2, false);
+//   c.fill();
+//
+// }
+//
+// draw();
 
-  var svg = createSvg('body', svgHeight, svgWidth);
-
-  // Create our initial D3 chart.
-  svg.selectAll('rect')
-     .data(frequencyData)
-     .enter()
-     .append('rect')
-     .attr('x', function (d, i) {
-        return i * (svgWidth / frequencyData.length);
-     })
-     .attr('width', svgWidth / frequencyData.length - barPadding);
-
-  // Continuously loop and update chart with frequency data.
-  function renderChart() {
-     requestAnimationFrame(renderChart);
-
-     // Copy frequency data to frequencyData array.
-     analyser.getByteFrequencyData(frequencyData);
-
-     // Update d3 chart with new data.
-     svg.selectAll('rect')
-        .data(frequencyData)
-        .attr('y', function(d) {
-           return svgHeight - d;
-        })
-        .attr('height', function(d) {
-           return d;
-        })
-        .attr('fill', function(d) {
-           return 'rgb(0, 0, ' + d + ')';
-        });
-  }
-
-  // Run the loop
-  renderChart();
-
-});
+requestAnimationFrame(animate);
